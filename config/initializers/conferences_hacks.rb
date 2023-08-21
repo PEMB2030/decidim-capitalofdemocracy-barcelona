7998d0e72a57e3d9a37e79159ab6e2f0de6be32b
Anna Topalidi <t.opalidisanna@gmail.com>
2023-08-21 08:21:06 +0200

    move initializers and overrides to initializers folder

--- config/initializers/conferences_hacks.rb
+++ config/initializers/conferences_hacks.rb
@@ -13,3 +13,67 @@ Decidim.menu :menu do |menu|
     menu.add_item item[:key], I18n.t(item[:key], scope: "decidim.conferences.custom_conference_types"), item[:url], options
   end
 end
+
+Rails.application.config.to_prepare do
+  Decidim::StatisticCell.include(Decidim::StatisticCellOverride)
+  Decidim::Conference.include(ConferenceOverride)
+  Decidim::Conferences::ConferencesController.include(ConferencesControllerOverride)
+end
+
+Rails.application.config do
+  initializer "capitalitat.homepage_content_blocks" do
+    config.to_prepare do
+      Decidim.content_blocks.register(:homepage, :active_processes) do |content_block|
+        content_block.cell = "decidim/content_blocks/active_processes"
+        content_block.settings_form_cell = "decidim/content_blocks/active_processes_settings_form"
+        content_block.public_name_key = "decidim.content_blocks.active_processes.name"
+
+        content_block.settings do |settings|
+          settings.attribute :button_text, type: :text, translated: true
+          settings.attribute :button_url, type: :text
+
+          (1..3).each do |i|
+            settings.attribute "link_url_#{i}".to_sym, type: :text
+            settings.attribute "link_text_#{i}".to_sym, type: :text, translated: true
+            settings.attribute "text_color_#{i}".to_sym, type: :string
+          end
+        end
+
+        (1..3).each do |i|
+          content_block.images << {
+            name: "image_#{i}".to_sym,
+            uploader: "Decidim::ActiveProcessesImageUploader"
+          }
+        end
+
+        content_block.default!
+      end
+
+      Decidim.content_blocks.register(:homepage, :extended_hero) do |content_block|
+        content_block.cell = "decidim/content_blocks/extended_hero"
+        content_block.settings_form_cell = "decidim/content_blocks/extended_hero_settings_form"
+        content_block.public_name_key = "decidim.content_blocks.extended_hero.name"
+
+        content_block.settings do |settings|
+          settings.attribute :welcome_title, type: :text, translated: true
+          settings.attribute :subtitle, type: :text, translated: true
+        end
+
+        content_block.images = [
+          {
+            name: :background_image,
+            uploader: "Decidim::HomepageImageUploader"
+          }
+        ]
+
+        content_block.default!
+      end
+    end
+  end
+
+  initializer "capitalitat.controller_overrides", after: "decidim.action_controller" do
+    config.to_prepare do
+      Decidim::Conference.include(ConferenceOverride)
+    end
+  end
+end
