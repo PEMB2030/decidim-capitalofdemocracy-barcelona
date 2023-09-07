d4c367fef024c1e0c596dc53c53f8d83c88789f8
Anna Topalidi <60363870+antopalidi@users.noreply.github.com>
2023-09-07 19:54:15 +0200

    Filter conferences by colors (#14)
    * Replace hashtags, Remove menu "triplication"
    
    * Replace hashtags
    
    * show buttons for existing conferences
    
    * refactoring
    
    * add original to deface
    
    * cells override
    
    * use a query string
    
    * add tests
    
    * add controller spec
    
    * add spec checksums
    
    * change default links color
    
    * active links
    
    * styles

--- spec/controllers/conferences_controller_spec.rb
+++ spec/controllers/conferences_controller_spec.rb
@@ -10,77 +10,47 @@ module Decidim
 
       let(:organization) { create(:organization) }
 
-      let!(:published1) { create(:conference, :published, organization: organization, hashtag: "actesciutat") }
-      let!(:published2) { create(:conference, :published, organization: organization, hashtag: "actesparalels") }
-      let!(:published_no_hashtag) { create(:conference, :published, organization: organization, hashtag: "") }
-      let!(:promoted) { create(:conference, :published, :promoted, organization: organization, hashtag: "actesciutat") }
-      let!(:promoted_no_hashtag) { create(:conference, :published, :promoted, organization: organization, hashtag: "") }
+      let!(:published1) { create(:conference, :published, organization: organization, hashtag: "ciutat") }
+      let!(:published2) { create(:conference, :published, organization: organization, hashtag: "activitat") }
+      let!(:published3) { create(:conference, :published, organization: organization, hashtag: "internacional") }
 
       before do
         request.env["decidim.current_organization"] = organization
-        allow(Rails.application.secrets).to receive(:custom_conference_types).and_return([{ url: "/conferences/city", hashtag: "actesciutat" }])
-      end
-
-      describe "published_conferences" do
-        context "when the custom hashtag exists" do
-          before do
-            request.env["PATH_INFO"] = "/conferences/city"
-            get :index
-          end
-
-          it "includes only conferences with the matching hashtag" do
-            expect(controller.published_conferences).to match_array([published1, promoted])
-          end
-        end
-
-        context "when the custom hashtag does not exist" do
-          it "includes all published conferences except those with custom hashtag" do
-            get :index
-
-            expect(controller.published_conferences).to match_array([published2, published_no_hashtag, promoted_no_hashtag])
-          end
-        end
       end
 
       describe "conferences" do
-        context "when the custom hashtag exists" do
-          before do
-            request.env["PATH_INFO"] = "/conferences/city"
-            get :index
-          end
-
-          it "includes only conferences with the matching hashtag" do
-            expect(controller.helpers.conferences).to match_array([published1, promoted])
-          end
-        end
-
-        context "when the custom hashtag does not exist" do
-          it "includes all conferences except those with custom hashtags" do
-            get :index
-
-            expect(controller.helpers.conferences).to match_array([published2, published_no_hashtag, promoted_no_hashtag])
-          end
+        it "includes published conferences" do
+          expect(controller.helpers.conferences).to match_array([published1, published2, published3])
         end
       end
 
-      describe "promoted_conferences" do
-        context "when the custom hashtag exists" do
-          before do
-            request.env["PATH_INFO"] = "/conferences/city"
-            get :index
-          end
-
-          it "includes only promoted conferences with the matching hashtag" do
-            expect(controller.helpers.promoted_conferences).to match_array([promoted])
-          end
+      describe "city conferences" do
+        before do
+          allow(Rails.application.secrets).to receive(:custom_conference_types).and_return([{ type: "city", hashtag: "ciutat" }])
         end
 
-        context "when the custom hashtag does not exist" do
-          it "includes all promoted conferences except those with custom hashtag" do
-            get :index
+        it "includes only published conferences with hashtag ciutat" do
+          expect(controller.helpers.conferences).to match_array([published1])
+        end
+      end
 
-            expect(controller.helpers.promoted_conferences).to match_array([promoted_no_hashtag])
-          end
+      describe "activity conferences" do
+        before do
+          allow(Rails.application.secrets).to receive(:custom_conference_types).and_return([{ type: "activity", hashtag: "activitat" }])
+        end
+
+        it "includes only published conferences with hashtag activitat" do
+          expect(controller.helpers.conferences).to match_array([published2])
+        end
+      end
+
+      describe "international conferences" do
+        before do
+          allow(Rails.application.secrets).to receive(:custom_conference_types).and_return([{ type: "international", hashtag: "internacional" }])
+        end
+
+        it "includes only published conferences with hashtag internacional" do
+          expect(controller.helpers.conferences).to match_array([published3])
         end
       end
     end
