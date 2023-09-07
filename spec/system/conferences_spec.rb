0afc191dcac6358869f8d3976820eae9b03a4c32
Anna Topalidi <60363870+antopalidi@users.noreply.github.com>
2023-09-07 19:54:15 +0200

    Filter conferences by colors (#14)
    * Replace hashtags, Remove menu "triplication"
    
    * Replace hashtags
    
    * show buttons for existing conferences
    
    * refactoring
    
    * add original to deface
    
    * cells override
    
    * use a query string
    
    * add tests
    
    * add controller spec
    
    * add spec checksums
    
    * change default links color
    
    * active links
    
    * styles

--- spec/system/conferences_spec.rb
+++ spec/system/conferences_spec.rb
@@ -4,111 +4,60 @@ require "rails_helper"
 
 describe "Visit conferences", type: :system do
   let!(:organization) { create :organization }
-  let!(:conference_city) { create :conference, :published, :promoted, organization: organization, hashtag: hashtag_city }
-  let!(:conference_global) { create :conference, :published, :promoted, organization: organization, hashtag: hashtag_global }
-  let!(:conference_parallel) { create :conference, :published, :promoted, organization: organization, hashtag: hashtag_parallel }
-  let(:hashtag_global) { "actesglobals" }
-  let(:hashtag_parallel) { "actesparalels" }
+  let!(:conference_activity) { create :conference, :published, promoted: false, organization: organization, hashtag: hashtag_activity }
+  let!(:conference_city) { create :conference, :published, promoted: false, organization: organization, hashtag: hashtag_city }
+  let!(:conference_international) { create :conference, :published, promoted: false, organization: organization, hashtag: hashtag_international }
+
+  let(:hashtag_activity) { "activitat" }
+  let(:hashtag_city) { "ciutat" }
+  let(:hashtag_international) { "internacional" }
+
+  let(:conferences) do
+    {
+      "activity" => conference_activity,
+      "city" => conference_city,
+      "international" => conference_international
+    }
+  end
 
   before do
     switch_to_host(organization.host)
+    visit decidim.root_path
+    click_link "Conferences"
   end
 
-  context "when visiting home page" do
-    let!(:hashtag_city) { "actesciutat" }
+  shared_examples "checks the conference link" do |type|
+    it "shows the #{type} conference link and its content" do
+      expected_text = I18n.t("decidim.conferences.custom_conference_types.#{type}")
 
-    before do
-      visit decidim.root_path
-    end
-
-    it "shows the original conference menu" do
-      within ".main-nav" do
-        expect(page).to have_content("Conferences")
-        expect(page).to have_link(href: "/conferences")
-      end
-    end
-
-    it "shows the city conference menu" do
-      within ".main-nav" do
-        expect(page).to have_content("City conferences")
-        expect(page).to have_link(href: "/conferences/city")
+      within "#conferences-grid" do
+        expect(page).to have_link(text: /#{expected_text}/i)
       end
 
-      click_link "City conferences"
+      click_link expected_text
 
-      expect(page).to have_content("1 CONFERENCE")
-      expect(page).to have_content(conference_city.title["en"], count: 2)
-      expect(page).not_to have_content(conference_global.title["en"])
-      expect(page).not_to have_content(conference_parallel.title["en"])
+      expect(page).to have_content(conferences[type].title["en"])
+      expect(page).to have_css(".card--conference", count: 1)
     end
+  end
 
-    it "shows the global conference menu" do
-      within ".main-nav" do
-        expect(page).to have_content("Conferences")
-        expect(page).to have_link(href: "/conferences")
-      end
+  describe "Checking each conference type link" do
+    conference_types_local = %w(activity city international)
 
-      click_link "Conferences"
-
-      expect(page).to have_content("1 CONFERENCE")
-      expect(page).to have_content(conference_global.title["en"], count: 2)
-      expect(page).not_to have_content(conference_city.title["en"])
-      expect(page).not_to have_content(conference_parallel.title["en"])
-    end
-
-    it "shows the parallel conference menu" do
-      within ".main-nav" do
-        expect(page).to have_content("Parallel conferences")
-        expect(page).to have_link(href: "/conferences/parallel")
-      end
-
-      click_link "Parallel conferences"
-
-      expect(page).to have_content("1 CONFERENCE")
-      expect(page).to have_content(conference_parallel.title["en"], count: 2)
-      expect(page).not_to have_content(conference_city.title["en"])
-      expect(page).not_to have_content(conference_global.title["en"])
+    conference_types_local.each do |type|
+      it_behaves_like "checks the conference link", type
     end
   end
 
   context "when hashtag with #" do
-    let!(:hashtag_city) { "#actesciutat" }
+    let!(:hashtag_city) { "#ciutat" }
 
-    before do
-      visit decidim.root_path
-    end
-
-    it "shows the city conference menu" do
-      within ".main-nav" do
-        expect(page).to have_content("City conferences")
-        expect(page).to have_link(href: "/conferences/city")
-      end
-
-      click_link "City conferences"
-
-      expect(page).to have_content("1 CONFERENCE")
-    end
+    it_behaves_like "checks the conference link", "city"
   end
 
   context "when hashtag contains spaces" do
-    let!(:hashtag_city) { " actesciutat " }
+    let!(:hashtag_city) { " ciutat " }
 
-    before do
-      visit decidim.root_path
-    end
-
-    it "shows the city conference menu" do
-      within ".main-nav" do
-        expect(page).to have_content("City conferences")
-        expect(page).to have_link(href: "/conferences/city")
-      end
-
-      click_link "City conferences"
-
-      expect(page).to have_content("1 CONFERENCE")
-      expect(page).to have_content(conference_city.title["en"])
-      expect(page).not_to have_content(conference_global.title["en"])
-      expect(page).not_to have_content(conference_parallel.title["en"])
-    end
+    it_behaves_like "checks the conference link", "city"
   end
 end
