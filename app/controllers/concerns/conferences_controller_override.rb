f92df43f038bfde191564b801ad9d69bb1f9c406
Anna Topalidi <t.opalidisanna@gmail.com>
2023-09-07 08:30:44 +0200

    refactoring

--- app/controllers/concerns/conferences_controller_override.rb
+++ app/controllers/concerns/conferences_controller_override.rb
@@ -4,13 +4,14 @@ module ConferencesControllerOverride
   extend ActiveSupport::Concern
 
   included do
-
     private
 
     def hashtag_for_current_url
-      @hashtag_for_current_url ||= Rails.application.secrets.custom_conference_types.find do |item|
-        item[:url] == request.path
-      end&.dig(:hashtag)
+      @hashtag_for_current_url ||= find_hashtag_for_current_url
+    end
+
+    def find_hashtag_for_current_url
+      Rails.application.secrets.custom_conference_types.find { |item| item[:url] == request.path }&.dig(:hashtag)
     end
 
     def published_conferences
@@ -21,10 +22,8 @@ module ConferencesControllerOverride
       @conferences ||= conferences_query(Decidim::Conferences::OrganizationPrioritizedConferences)
     end
 
-    alias_method :collection, :conferences
-
-    def promoted_conferences
-      @promoted_conferences ||= hashtag_for_current_url ? promoted_filtered_by_hashtag(conferences) : promoted_conferences_without_custom_hashtag(Decidim::Conference.promoted)
+    def collection
+      conferences
     end
 
     def custom_hashtags
@@ -32,29 +31,8 @@ module ConferencesControllerOverride
     end
 
     def conferences_query(conference_class)
-      conferences = conference_class.new(current_organization, current_user)
-      hashtag_for_current_url ? conferences_with_custom_hashtag_filter(conferences) : conferences_without_custom_hashtag(conferences)
-    end
-
-    def conferences_with_custom_hashtag_filter(conferences)
-      conferences.query.merge(Decidim::Conference.filtered_by_hashtag(hashtag_for_current_url))
-    end
-
-    def conferences_without_custom_hashtag(conferences)
-      conferences.query.to_a.reject do |conference|
-        conference_hashtags = conference.hashtag.split
-        (conference_hashtags & custom_hashtags).any?
-      end
-    end
-
-    def promoted_filtered_by_hashtag(conferences)
-      conferences.merge(Decidim::Conference.promoted.filtered_by_hashtag(hashtag_for_current_url))
-    end
-
-    def promoted_conferences_without_custom_hashtag(conferences)
-      conferences.select do |conference|
-        conference_hashtags = conference.hashtag.split
-        (conference_hashtags & custom_hashtags).empty?
+      conference_class.new(current_organization, current_user).query.tap do |query|
+        query.merge!(Decidim::Conference.filtered_by_hashtag(hashtag_for_current_url)) if hashtag_for_current_url
       end
     end
   end
