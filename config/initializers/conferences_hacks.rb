f22d0d021b2a6e17270df5bd126713d3ee48f3ed
Anna Topalidi <60363870+antopalidi@users.noreply.github.com>
2023-09-12 20:38:27 +0200

    Create a new home content block with partner logos (#16)
    * add content block
    
    * add logos
    
    * add logos
    
    ---------
    
    Co-authored-by: Ivan Vergés <ivan@pokecode.net>

--- config/initializers/conferences_hacks.rb
+++ config/initializers/conferences_hacks.rb
@@ -62,3 +62,9 @@ Decidim.content_blocks.register(:homepage, :promo_video) do |content_block|
   content_block.public_name_key = "decidim.content_blocks.promo_video.name"
   content_block.default!
 end
+
+Decidim.content_blocks.register(:homepage, :partner_logos) do |content_block|
+  content_block.cell = "decidim/content_blocks/partner_logos"
+  content_block.public_name_key = "decidim.content_blocks.partner_logos.name"
+  content_block.default!
+end
