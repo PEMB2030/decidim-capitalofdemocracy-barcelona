af17efae9bd7d17ed3301b16bc22708d63cecfef
Ivan Vergés <ivan@pokecode.net>
2023-09-21 13:02:14 +0200

    prevent non conferences to use hashtag hack

--- app/cells/concerns/decidim/conference_cell_extensions.rb
+++ app/cells/concerns/decidim/conference_cell_extensions.rb
@@ -5,6 +5,8 @@ module Decidim
     extend ActiveSupport::Concern
 
     def conference_class(model)
+      return "" unless model.is_a?(Decidim::Conference)
+
       custom_conference_types = Rails.application.secrets.custom_conference_types || []
       custom_conference_types.each do |item|
         return item[:key] if model.hashtag.include?(item[:hashtag])
