026580aa86571c527649866458f85380f2d6cb0f
Ivan Vergés <ivan@pokecode.net>
2023-10-16 13:43:03 +0200

    fix order again

--- app/models/concerns/conference_override.rb
+++ app/models/concerns/conference_override.rb
@@ -4,6 +4,9 @@ module ConferenceOverride
   extend ActiveSupport::Concern
 
   included do
+    scope :past, -> { where(arel_table[:end_date].lt(Date.current)) }
+    scope :upcoming, -> { where(arel_table[:end_date].gteq(Date.current)) }
+
     def self.filtered_by_hashtag(hashtag)
       where("(CONCAT(' ', hashtag, ' ') ILIKE ?) OR (CONCAT(' ', hashtag, ' ') ILIKE ?)", "% #{hashtag} %", "% ##{hashtag} %")
     end
