d9f35afda1ed485fb8023c63237537d3bb51ec0a
elviabth <ejbenedith@gmail.com>
2024-01-31 15:44:43 +0100

    pass rubocop

--- app/controllers/concerns/conferences_controller_override.rb
+++ app/controllers/concerns/conferences_controller_override.rb
@@ -3,46 +3,46 @@
 module ConferencesControllerOverride
   extend ActiveSupport::Concern
 
-    included do
-      private
+  included do
+    private
 
-      def hashtag_for_current_type
-        @hashtag_for_current_type ||= find_hashtag_for_current_type
-      end
+    def hashtag_for_current_type
+      @hashtag_for_current_type ||= find_hashtag_for_current_type
+    end
 
-      def find_hashtag_for_current_type
-        type = params[:type]
-        Rails.application.secrets.custom_conference_types.find { |item| item[:key] == type }&.dig(:hashtag)
-      end
+    def find_hashtag_for_current_type
+      type = params[:type]
+      Rails.application.secrets.custom_conference_types.find { |item| item[:key] == type }&.dig(:hashtag)
+    end
 
-      def published_conferences
-        @published_conferences ||= conferences_query(Decidim::Conferences::OrganizationPublishedConferences)
-      end
+    def published_conferences
+      @published_conferences ||= conferences_query(Decidim::Conferences::OrganizationPublishedConferences)
+    end
 
-      def conferences
-        @conferences ||= conferences_query(Decidim::Conferences::OrganizationPrioritizedConferences)
-      end
+    def conferences
+      @conferences ||= conferences_query(Decidim::Conferences::OrganizationPrioritizedConferences)
+    end
 
-      def collection
-        sorted_conferences
-      end
+    def collection
+      sorted_conferences
+    end
 
-      def sorted_conferences
-        Decidim::Conferences::ConferenceSort.new(conferences).sort
-      end
+    def sorted_conferences
+      Decidim::Conferences::ConferenceSort.new(conferences).sort
+    end
 
-      def promoted_conferences
-        @promoted_conferences ||= conferences.promoted
-      end
+    def promoted_conferences
+      @promoted_conferences ||= conferences.promoted
+    end
 
-      def custom_hashtags
-        @custom_hashtags ||= Rails.application.secrets.custom_conference_types.map { |item| item[:hashtag] }
-      end
+    def custom_hashtags
+      @custom_hashtags ||= Rails.application.secrets.custom_conference_types.map { |item| item[:hashtag] }
+    end
 
-      def conferences_query(conference_class)
-        conference_class.new(current_organization, current_user).query.tap do |query|
-          query.merge!(Decidim::Conference.filtered_by_hashtag(hashtag_for_current_type)) if hashtag_for_current_type
-        end
+    def conferences_query(conference_class)
+      conference_class.new(current_organization, current_user).query.tap do |query|
+        query.merge!(Decidim::Conference.filtered_by_hashtag(hashtag_for_current_type)) if hashtag_for_current_type
       end
+    end
   end
 end
