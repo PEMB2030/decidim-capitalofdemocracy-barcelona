a924f4cd0670a75fdceee575a4b4185cd9e9bdcb
elviabth <ejbenedith@gmail.com>
2024-05-15 13:25:23 +0200

    disable rubocop in conference_helper_override

--- app/helpers/concerns/conference_helper_override.rb
+++ app/helpers/concerns/conference_helper_override.rb
@@ -6,9 +6,10 @@ module ConferenceHelperOverride
   included do
     alias_method :decidim_conference_nav_items, :conference_nav_items
 
+    # rubocop:disable Style/SingleArgumentDig
     def conference_nav_items(participatory_space)
       @conference_nav_items ||= begin
-        divisor = Rails.application.secrets.speakers_divisor[participatory_space.slug.to_sym]
+        divisor = Rails.application.secrets.speakers_divisor.dig(participatory_space.slug.to_sym)
         items = decidim_conference_nav_items(participatory_space)
         items.first[:name] = I18n.t("capital.speakers_divisor.#{divisor.first[:name]}") if participatory_space.speakers.exists? && divisor && divisor&.first&.[](:name)
         items
